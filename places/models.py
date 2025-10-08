4b08b1dffc24c05877e43f48713ad3dc2b5db53e
Evgeny <evgenygorokhovg@yahoo.com>
2025-10-08 12:46:27 +0300

    Add model for images.

--- places/models.py
+++ places/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.db.models import PositiveSmallIntegerField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=255)
-    short_description = models.TextField(blank=True)
-    long_description = models.TextField(blank=True)
-    latitude = models.FloatField(blank=True, null=True)
-    longitude = models.FloatField(blank=True, null=True)
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    short_description = models.TextField(blank=True, verbose_name='краткое описание')
+    long_description = models.TextField(blank=True, verbose_name='расширенное описание')
+    latitude = models.FloatField(blank=True, null=True, verbose_name='широта')
+    longitude = models.FloatField(blank=True, null=True, verbose_name='долгота')
 
     def __str__(self):
         return self.title
 
+
+class Image(models.Model):
+    order = PositiveSmallIntegerField(verbose_name='номер изображения')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='место')
+    image = models.ImageField(upload_to='places/images/', verbose_name='изображение')
+
+    def __str__(self):
+        return f'{self.order} - {self.place.title}'
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -116,7 +116,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -124,6 +123,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
